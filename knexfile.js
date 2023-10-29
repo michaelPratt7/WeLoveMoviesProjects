24805a612565c454a581aefdfa9ea7c98ef09f26
Michael Pratt <mrpratt7751@gmail.com>
2023-10-29 17:38:57 -0400

    db changes

--- knexfile.js
+++ knexfile.js
@@ -5,7 +5,7 @@ const {DATABASE_URL} = process.env;
 module.exports = {
   development: {
     client: "postgresql",
-    connection: DATABASE_URL,
+    connection: "postgres://wpkabeou:B7ngkPMECqBNN9NXZ8sHbJD-9MINr5_w@suleiman.db.elephantsql.com/wpkabeou",
     pool: { min: 0, max: 5 },
     migrations: {
       directory: path.join(__dirname, "src", "db", "migrations"),
@@ -17,7 +17,7 @@ module.exports = {
 
   production: {
     client: "postgresql",
-    connection: DATABASE_URL,
+    connection: "postgres://wpkabeou:B7ngkPMECqBNN9NXZ8sHbJD-9MINr5_w@suleiman.db.elephantsql.com/wpkabeou",
     pool: { min: 0, max: 5 },
     migrations: {
       directory: path.join(__dirname, "src", "db", "migrations"),
